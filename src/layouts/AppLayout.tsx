04e55d36ee26a2ad3d9f122bdc67923b84b254b0
Carlos Montoya <car.ar.16@hotmail.com>
2025-05-20 15:08:52 -0700

    first commit

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import DevTree from "../components/DevTree";
+import { getUser } from "../api/DevTreeapi";
+
+export default function AppLayout() {
+
+    const { data, isLoading, isError } = useQuery({
+        queryFn: getUser,
+        queryKey: ['user'],
+        retry: 2,
+        refetchOnWindowFocus: false,
+    })
+    
+    if(isLoading) return <div className="w-full h-screen flex items-center justify-center">Cargando...</div>
+    if(isError) return <Navigate to={'/auth/login'} />
+
+    if(data) return <DevTree data={data} />
+}
---
+++ src/views/HandleView.tsx
@@ -0,0 +1,20 @@
+import { Navigate, useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getUserByHandle } from '../api/DevTreeapi'
+import HandleData from '../components/HandleData'
+
+
+export default function HandleView() {
+
+    const params = useParams()
+    const handle = params.handle!
+    const { data, error, isLoading } = useQuery({
+        queryFn: () => getUserByHandle(handle),
+        queryKey: ['handle', handle],
+        retry: 0
+    })
+
+    if(isLoading) return "Cargando..."
+    if(error) return <Navigate to={'/404'} />
+    if(data) return <HandleData data={data} />
+}
